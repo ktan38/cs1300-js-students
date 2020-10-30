a9529f8e3f5b288713423e6e1f75b465237d44e0
Kendrick Tan <kendricktan38@gmail.com>
2020-10-30 17:19:42 +0800

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?filter[author]=Sm.&token=yrSJoHRw9E3yA01cgbSvaBSZoLc4MXSrmO-kHrdd3XM";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,27 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      var edible  = JSON.parse(request.response);
+      edible = edible.data;
+      edible.forEach(printPlant);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+function printPlant(plantData){
+  var div = document.createElement('div');
+  div.className = "grid-item";
+  var url = plantData.image_url
+  if (url !== null) {
+    var img = document.createElement('img'); 
+    img.src = url;
+    div.appendChild(img);
+  }
+  var tag = document.createElement("p");
+  var text = document.createTextNode(plantData.scientific_name);
+  tag.appendChild(text);
+  div.appendChild(tag);
+  var element = document.getElementById("parent");
+  element.appendChild(div);
+}
